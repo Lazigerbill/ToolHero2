800735a44d6d3627b40be79864657dc6131a3c36
Tabish <tabishiqbal@outlook.com>
2014-10-04 20:29:32 -0400

    added css, created header partial, added list view to tools and fixed tagging

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -3,6 +3,7 @@ class Tool < ActiveRecord::Base
 	has_many :tool_transactions
 	belongs_to :manufacturer
 
+	acts_as_taggable
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :dashboards
   resources :tools
 
+  get 'tags/:tag', to: 'tools#index', as: :tag
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
